3d2ecc465aea30e56f4f603d3110dc4983729c78
maryamsaye <marynsaye@gmail.com>
2025-02-17 18:16:56 +0100

    Added man.jsx

--- src/Home/Ladies.jsx
+++ src/Home/Ladies.jsx
@@ -9,11 +9,14 @@ const Ladies = () => {
         <div>
             <div className="group2">
                 <img src={group2}alt="" className="ladies" />
-                <h1>Lorem ipsum dolor sit amet consectetur.</h1>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt impedit cumque deleniti facilis, reprehenderit eaque! Architecto aut eveniet nulla officia?</p>
+                <div className="ipsum">
+                    <h1>Lorem ipsum dolor sit amet consectetur.</h1>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt impedit cumque deleniti facilis, reprehenderit eaque! Architecto aut eveniet nulla officia?</p>
+                    <button className="black2">Learn More</button>
+                </div>
 
         </div>
-            <button className="black2">Learn More</button>
+            
         </div>
     )
 }
