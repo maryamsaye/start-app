27c5741a6a741c6a8615fe5c1bf5021ebe9f79bc
maryamsaye <marynsaye@gmail.com>
2025-02-21 00:05:16 +0100

    initial commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import start from "../../assets/start.svg";
+import hamburger from "../../assets/hamburger.svg"
+
+function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <section className="nav-header">
+      <div className="navbar">
+        <Link to="/" className="logo-container">
+          <img src={start} alt="Logo" className="logo" />
+        </Link>
+        <img
+          src={hamburger}
+          alt="Menu"
+          className="menu-icon"
+          onClick={toggleMenu}
+        />
+      </div>
+      <nav className={`nav-links ${menuOpen ? "open" : ""}`}>
+        <ul>
+          <li>
+            <Link to="/" onClick={() => setMenuOpen(false)}>Home</Link>
+          </li>
+          <li>
+            <Link to="/portfolio" onClick={() => setMenuOpen(false)}>Portfolio</Link>
+          </li>
+          {/* <li>
+            <Link to="/services" onClick={() => setMenuOpen(false)}>Our Services</Link>
+          </li>
+          <li>
+            <Link to="/contact" onClick={() => setMenuOpen(false)}>Contact</Link>
+          </li> */}
+        </ul>
+      </nav>
+    </section>
+  );
+}
+
+export default Navbar;
--- src/mail.jsx
+++ src/mail.jsx
@@ -10,14 +10,13 @@ const Mail = () => {
       <div className='mail-con'>      <img src={mailImage} alt="mail" className='mail' />
       </div>
       <div className='accent'>
-      <h1 className='team-text1'>Lemon ipsum dolor sit amet consectetur</h1>
-      <h3 className='team-text'>Lemon ipsum, dolor sit amet consectetur adipisicing elit. Suscipit  meno hit quos, ab, dolor aperiam nobis cum est eos error ipsum,volimptate culpa nesciunt delectus  iste?</h3>
+      <h1 className='team-text1'>Lorem ipsum dolor sit amet consectetur</h1>
+      <h3 className='team-text'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit  meno hit quos, ab, dolor aperiam nobis cum est eos error ipsum,volimptate culpa nesciunt delectus  iste?</h3>
       <button className="learn-button1">Learn More</button>
       </div>
       
     </div>
     </>
-
   );
 };
 
